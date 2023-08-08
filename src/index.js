04e1e1866f8aa3b794a59a950832026846a9a16a
Banessa <maebanessa@gmail.com>
2023-08-08 16:24:46 +0900

    1

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ export default class App extends Component {
 
   state = {
     todoData: [
-      { id: 1, label: 'Drink Coffee', important: false, done: false },
-      { id: 2, label: 'Learn React', important: true, done: false },
-      { id: 3, label: 'Make Awesome App', important: false, done: false },
+      // { id: 1, label: 'Drink Coffee', important: false, done: false },
+      // { id: 2, label: 'Learn React', important: true, done: false },
+      // { id: 3, label: 'Make Awesome App', important: false, done: false },
     ],
     // term:'',
     filter: 'all',
